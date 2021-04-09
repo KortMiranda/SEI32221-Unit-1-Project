bf8131f41e54c6b9002810c7cd68d6373118657f
kortmiranda <kortneymiranda@outlook.com>
2021-04-09 11:59:00 -0500

    added input bar that checks if value is true or false

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 const genButton = document.querySelector('.genButton')
-const charHeader = document.querySelector('.charHeader')
-
+const charFront = document.querySelector('.charFront')
+const charBack = document.querySelector('.charBack')
+const prompt = document.querySelector('h4')
+const promptText = document.querySelector('.prompt')
+const inputText = document.querySelector('#inputBar')
+const submitButton = document.querySelector('#submitButton')
 
 async function getData(e) {
     e.preventDefault()
-    let randomId = Math.floor(Math.random() * 55)
+    let randomId = Math.floor(Math.random() * 80) //has be declared within the function before the url
     const url = `https://swapi.dev/api/people/${randomId}`
     fetch(url)
     .then(res => {
@@ -13,11 +17,24 @@ async function getData(e) {
     })
     
     .then(data => {
-        randomId
-        charHeader.innerText = data.name
+        //front of card
+        charFront.innerText = data.name
+
+        //back of card
+        charBack.innerText = data.name
+
         console.log("success!", data)
     })
     .catch(err => console.log('something went wrong', err))
 }
 
+const checkAnswer= () => {
+    if(inputText.value.toLowerCase() === charBack.innerHTML.toLowerCase() || inputText.value === charBack.innerHTML) {
+        console.log("true") 
+    } else {
+        console.log("false")
+    }
+    // console.log(inputText.value)
+}
 genButton.addEventListener('click', getData)
+submitButton.addEventListener('click', checkAnswer)
